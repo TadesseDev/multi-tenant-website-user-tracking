88e8c3efac2379c83e851a71a7e027cf050f04b5
Tadesse D <Tadessedev@gmail.com>
2026-04-14 09:27:46 +0400

    fix: update idempotency exception filter file

--- src/common/exceptions/idempotency.exception.ts
+++
@@ -1,7 +0,0 @@
-import { ConflictException } from '@nestjs/common';
-
-export class IdempotencyException extends ConflictException {
-  constructor(message = 'Event with this ID already processed') {
-    super(message);
-  }
-}
